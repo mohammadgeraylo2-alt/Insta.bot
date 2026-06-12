ce154571a28eba1b3a15d19a750e7e65517cd68e
mohammadgeraylo2-alt <mohammadgeraylo2@gmail.com>
2026-06-13 01:26:05 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,32 @@
-from telegram import Update
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ApplicationBuilder, MessageHandler, CommandHandler, filters, ContextTypes
 import yt_dlp
 import os
 
 TOKEN = os.environ["BOT_TOKEN"]
+CHANNEL = "@downloader_hamechi"
+
+async def is_member(bot, user_id):
+    try:
+        member = await bot.get_chat_member(CHANNEL, user_id)
+        return member.status in ["member", "administrator", "creator"]
+    except:
+        return False
+
+async def not_joined_message(update):
+    keyboard = [[InlineKeyboardButton("عضویت در کانال 📢", url=f"https://t.me/downloader_hamechi")]]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    await update.message.reply_text(
+        "⛔️ برای استفاده از ربات باید عضو کانال ما بشی!\n\n"
+        "بعد از عضویت دوباره امتحان کن 👇",
+        reply_markup=reply_markup
+    )
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user_id = update.message.from_user.id
+    if not await is_member(context.bot, user_id):
+        await not_joined_message(update)
+        return
     await update.message.reply_text(
         "👋 سلام خوش اومدی!\n\n"
         "با این ربات میتونی پست، ریلز و ویدیوهای اینستاگرام رو دانلود کنی 📥\n\n"
@@ -13,6 +34,11 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     )
 
 async def handle_link(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user_id = update.message.from_user.id
+    if not await is_member(context.bot, user_id):
+        await not_joined_message(update)
+        return
+
     url = update.message.text.strip()
 
     if "instagram.com" not in url:
